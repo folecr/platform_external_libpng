b3f94f6df636073ae1c90b8ce5f8dd594becbab8
folecr <folecr@gmail.com>
2012-02-10 17:41:24 -0800

    Static lib for Cocos

--- Android.mk
+++ Android.mk
@@ -1,9 +1,16 @@
 LOCAL_PATH:= $(call my-dir)
 
-# We need to build this for both the device (as a shared library)
-# and the host (as a static library for tools to use).
+# ------------------------------------------------------------------
+# Add static library for Cocos
+# ------------------------------------------------------------------
 
-common_SRC_FILES := \
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos_libpng_static
+
+LOCAL_MODULE_FILENAME := libpng
+
+LOCAL_SRC_FILES :=\
 	png.c \
 	pngerror.c \
 	pnggccrd.c \
@@ -22,56 +29,10 @@ common_SRC_FILES := \
 	pngwtran.c \
 	pngwutil.c
 
-common_CFLAGS := -fvisibility=hidden ## -fomit-frame-pointer
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
-ifeq ($(HOST_OS),windows)
-  ifeq ($(USE_MINGW),)
-    # Case where we're building windows but not under linux (so it must be cygwin)
-    # In this case, gcc cygwin doesn't recognize -fvisibility=hidden
-    $(info libpng: Ignoring gcc flag $(common_CFLAGS) on Cygwin)
-    common_CFLAGS := 
-  endif
-endif
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
-common_C_INCLUDES += 
-
-common_COPY_HEADERS_TO := libpng
-common_COPY_HEADERS := png.h pngconf.h pngusr.h
-
-# For the host
-# =====================================================
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES) external/zlib
-
-LOCAL_MODULE:= libpng
-
-LOCAL_COPY_HEADERS_TO := $(common_COPY_HEADERS_TO)
-LOCAL_COPY_HEADERS := $(common_COPY_HEADERS)
-
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-
-# For the device
-# =====================================================
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES) \
-	external/zlib
-LOCAL_SHARED_LIBRARIES := \
-	libz
-
-LOCAL_MODULE:= libpng
-
-LOCAL_COPY_HEADERS_TO := $(common_COPY_HEADERS_TO)
-LOCAL_COPY_HEADERS := $(common_COPY_HEADERS)
+LOCAL_LDLIBS := -lz
 
 include $(BUILD_STATIC_LIBRARY)
-
-
